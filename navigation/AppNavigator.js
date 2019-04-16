04892e5dcd5fe8be817cece143a109d985bcc8d6
Matias <matias_mateu@icloud.com>
2019-04-16 15:23:53 +0200

    Basic Routing

---
+++ navigation/AppNavigator.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { createBottomTabNavigator } from "react-navigation";
+import Ionicons from 'react-native-vector-icons/Ionicons';
+// containers 
+import WorkOrders from '../src/containers/WorkOrders'
+
+export const TabNavigator = createBottomTabNavigator(
+  {
+  WorkOrders: WorkOrders,
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ tintColor }) => {
+        const { routeName } = navigation.state;
+        let IconComponent = Ionicons;
+        let iconName;
+        if (routeName === 'WorkOrders') {
+          iconName = `ios-build`;
+        } 
+        return <IconComponent name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: 'blue',
+      inactiveTintColor: 'gray',
+    },
+  }
+);
